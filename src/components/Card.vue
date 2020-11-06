ccd43a7c0927f1765d560eb72f68d2bccb81c17d
manuelmerkel <34129346+manuelmerkel@users.noreply.github.com>
2020-11-06 12:51:25 +0100

    Added protocols folder + added first protocol. Added scripts for devserver + lintfix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,12 @@
 <template>
-    <div>
-    </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'Card',
-  props: ['release', 'index'],
-}
+  name: "Card",
+  props: ["release", "index"]
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
