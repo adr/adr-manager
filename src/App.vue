18913bc59b0f366c2072a9f69ce7b975206df0b6
manuelmerkel <34129346+manuelmerkel@users.noreply.github.com>
2020-11-06 12:51:25 +0100

    Added protocols folder + added first protocol. Added scripts for devserver + lintfix

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,44 @@
 <template>
   <div id="app">
- <!-- added a navbar -->
-   <nav class="navbar navbar-dark bg-dark">
-     <span class="navbar-brand mb-0 h2">ADR-Manager</span>
-   </nav>
+    <!-- added a navbar -->
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h2">ADR-Manager</span>
+    </nav>
 
-   <!-- wrapped the data component in a container -->
-   <div class="container">
-   <!-- inject the Data component -->
-     <Data/>
-   </div>
-
- </div>
+    <!-- wrapped the data component in a container -->
+    <div class="container">
+      <!-- inject the Data component -->
+      <Data />
+    </div>
+  </div>
 </template>
 
 <script>
- // import the data component
-import Data from './components/Data.vue'
+// import the data component
+import Data from "./components/Data.vue";
 
 export default {
-    name: 'app',
-    components: {
-        Data
-    }
-}
+  name: "app",
+  components: {
+    Data
+  }
+};
 </script>
 
 <style>
-  /* global styles */
-body{
-    background-color: #FEFEFE
+/* global styles */
+body {
+  background-color: #fefefe;
 }
 
-.asset{
-    word-break: break-all;
+.asset {
+  word-break: break-all;
 }
 
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
 }
 </style>
