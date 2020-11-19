15c8c0d730e2da87d402600fdf917512f81d97c7
st155131 <st155131@stud.uni-stuttgart.de>
2020-11-19 22:21:25 +0100

    Added github authorisation

--- src/components/UsefulComponents/GitLogIn.vue
+++ src/components/UsefulComponents/GitLogIn.vue
@@ -1,24 +1,7 @@
 <template>
   <div>
-    <v-btn :href="githubAuthorizeUrl">
+    <v-btn href="https://github.com/login/oauth/authorize?client_id=7399c989ae7246112a4d&scope=repo">
       Log In with GitHub
     </v-btn>
   </div>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      text: 'Text',
-      githubAuthorizeUrl: 'https://github.com/login/oauth/authorize?client_id=2a2586230dd928a44291'
-    }),
-    methods: {
-      authorizeToGitHub() {
-        window.location.href = this.githubAuthorizeUrl
-      }
-    }
-  };
-</script>
-
-<style>
-</style>
