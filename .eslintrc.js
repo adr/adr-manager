b4409d4f257cb04f4d183825bdd3c68030b0877c
st155131 <st155131@stud.uni-stuttgart.de>
2020-11-24 16:33:30 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+  },
+};
