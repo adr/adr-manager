f27ebea81f8613d86ad97639bfc8523b6ca0e44d
st155131 <st155131@stud.uni-stuttgart.de>
2020-11-24 16:33:30 +0100

    Added eslint

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
-import router from './router'
+import router from './router';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
--- src/test/exampleTest.js
+++ src/test/exampleTest.js
@@ -1,5 +1,10 @@
+/**
+ * Only for test purpose.
+ * @param {number} a The first number
+ * @param {number} b The second number
+ * @return {number} The sum
+ */
 function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
-  
+  return a + b;
+}
+module.exports = sum;
