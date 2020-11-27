520db2933bb324033f3bd17236416f15281069b2
Daniel Abajirov <abajirovdaniel@gmail.com>
2020-11-27 11:51:15 +0100

    Fixed filerepos

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,10 +1,11 @@
 /* This file contains any calls to the backend. */
 
 import axios from 'axios'
+// import Pizzly from 'pizzly-js';
 
 // API-URLs
 //const reposPath = 'http://localhost:5000/repos'
-const treePath = 'http://localhost:5000/tree'
+// const treePath = 'http://localhost:5000/tree'
 const rawPath = 'http://localhost:5000/rawcontent'
 
 // API-Calls (functions return promises)
@@ -15,7 +16,6 @@ const rawPath = 'http://localhost:5000/rawcontent'
  * An example of the returned JSON structure can be found at 'https://api.github.com/users/adr/repos'
  */
 export async function loadRepositoryList(user, pizzly) {
-  
   return pizzly
         .integration("github")
         .auth(user)
@@ -33,17 +33,25 @@ export async function loadRepositoryList(user, pizzly) {
  * @param {String} repoFullName - the full name of the repository, e.g. 'adr/madr'
  * @param {String} branch - the name of the branch, e.g. 'master' 
  */
-export async function loadFileTreeOfRepository(repoFullName, branch) {
-  const formData = new FormData();
-  formData.append('full_name', repoFullName);
-  formData.append('branch', branch);
-  return axios.post(treePath, formData)
+export async function loadFileTreeOfRepository(repoFullName, branch, user, pizzly) {
+  //const formData = new FormData();
+  //formData.append('full_name', repoFullName);
+  //formData.append('branch', branch);
+  return pizzly
+    .integration("github")
+    .auth(user)
+    .get("/repos/" + repoFullName + "/git/trees/" + branch + "?recursive=1")
+    .then(response => response.json())
+    .catch((err) => {
+      console.log(err)
+  });
+  /**return axios.post(treePath, formData)
     .then(({ data }) => {
       return data.tree;
     })
     .catch((err) => {
       console.log(err)
-    });
+    });*/
 }
 
 /**
@@ -108,7 +116,8 @@ function decodeUnicode(str) {
  * @param {Array[Object]} loadRepositoryList - each array entry must have the attributes fullName and branch
  * @returns an array of repositories
  */
-export async function loadAllRepositoryContent(repoList) {
+export async function loadAllRepositoryContent(repoList, pizzly, user) {
+  
   return repoList.map((repo) => {
     let repoFullName = repo.fullName
     let branch = repo.branch
@@ -117,9 +126,9 @@ export async function loadAllRepositoryContent(repoList) {
       activeBranch: branch,
       adrs : []
     }
-    loadFileTreeOfRepository(repoFullName, branch)
+    loadFileTreeOfRepository(repoFullName, branch, user, pizzly)
       .then((data) => {
-        let adrList =  data.filter((file) =>  { // Find all files in the folder 'docs/adr' or 'doc/adr'
+        let adrList =  data.tree.filter((file) =>  { // Find all files in the folder 'docs/adr' or 'doc/adr'
           return file.path.startsWith('docs/adr/') || file.path.startsWith('doc/adr/') // Allow docs/adr and doc/adr as path .. maybe change this to demand mutual exclusion.
         })
         console.log('adrList', adrList)
