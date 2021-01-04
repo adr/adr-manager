0621f6187abc7a493866c23e91cbf25f2ff4d29e
Daniel Abajirov <abajirovdaniel@gmail.com>
2021-01-04 20:22:20 +0100

    Check parser

--- cypress/integration/adrManagerTest/Parser.js
+++ cypress/integration/adrManagerTest/Parser.js
@@ -20,5 +20,9 @@ context("Test new adr function", () => {
         
         cy.contains(' Markdown Preview ').click();
         cy.contains(' Raw Markdown ').click();
+        cy.get('[data-cy=markdownText]').click().type("# ADR-Manager   ## Project setup   ```  npm install   ### Compiles and hot-reloads for development ```   npm run serve ``` ### Compiles and minifies for production ```  npm run build # ADR Manager Research Project > All artefacts related to a research project from the University of Stuttgart to propose a tool-supported approach for the efficient creation and management"
+        + " of [architectural decision records (ADRs)](https://adr.github.io) via a graphical user interface (GUI)"
+        );
+        cy.contains(' Convert ').click();
     });
 })
