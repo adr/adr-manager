686e1bc6cbd0d80e89bcc796f110a678c005d38d
Daniel Abajirov <abajirovdaniel@gmail.com>
2021-01-04 17:24:49 +0100

    Added code coverage

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+// cypress/support/index.js
+import '@cypress/code-coverage/support'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
