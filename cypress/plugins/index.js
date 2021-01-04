33931214c78ada97ee27767e5c43b8440dfc2b82
Daniel Abajirov <abajirovdaniel@gmail.com>
2021-01-04 16:46:06 +0100

    Try to fix the crash on chromium with cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,12 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      args.push('--disable-dev-shm-usage')
+      return args
+    }
+
+    return args
+  })
 }
