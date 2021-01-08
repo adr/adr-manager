d050615f185a58bd553f745b09a2aefd8472d4f4
unknown <st155131@stud.uni-stuttgart.de>
2021-01-08 20:57:32 +0100

    Changed test. Changed latency dialog from error to warning

--- cypress/integration/adrManagerTest/EmpiricalAnalysis.js
+++ cypress/integration/adrManagerTest/EmpiricalAnalysis.js
@@ -11,29 +11,45 @@ context("Should test empirical add and open function of adr-manager", () => {
 
         cy.intercept('GET', '**/user/repos**').as('getRepos');
         cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
+        cy.writeFile('cypress/fixtures/CounterDiffPerRepo.json', { counter: "0" });
+        cy.writeFile('cypress/fixtures/CounterAdrsPerRepo.json', { counter: "0" });
     })
 
     it('Should test empirical add and open function of jabref repo', () => {
-        cy.writeFile('cypress/fixtures/CounterDiff.json', { counter: "0" });
-        cy.writeFile('cypress/fixtures/CounterAdrs.json', { counter: "0" });
+        cy.writeFile('cypress/fixtures/CounterDiffAllRepos.json', { counter: "0" });
+        cy.writeFile('cypress/fixtures/CounterAdrsAllRepos.json', { counter: "0" });
         cy.get('[data-cy=listRepo]').should('have.length', 5).eq(0).click();
         cy.get('[data-cy=addRepoDialog]').click();
         cy.get('[data-cy=adrList]').should('have.length', 21).each(($adr, index, $adrs) => {
             cy.get($adr).click();
-            cy.get('body').then($button => {
-                if ($button.find('[data-cy=acceptDiv]').length > 0) {
+            cy.get('header').then(($a) => {
+                cy.log('text', $a.text())
+                if ($a.text().includes('Convert')) {
                     cy.log('visible')
-                    cy.readFile('cypress/fixtures/CounterDiff.json').then((counter) => {
-                        cy.writeFile('cypress/fixtures/CounterDiff.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
                     })
-                } else cy.log('not visible')
-                cy.readFile('cypress/fixtures/CounterAdrs.json').then((counter) => {
-                    cy.writeFile('cypress/fixtures/CounterAdrs.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffAllRepos.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    })
+                } else cy.log('1not visible')
+                cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                })
+                cy.readFile('cypress/fixtures/CounterAdrsAllRepos.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
                 })
             })
         })
         cy.get('[data-cy=removeRepo]').click();
         cy.get('[data-cy=removeRepoBtn]').click();
+
+        cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
+            cy.log('diff', counter.counter)
+        })
+        cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+            cy.log('adr', counter.counter)
+        })
     });
 
     it('Should test empirical add and open function of adr-tools repo', () => {
@@ -49,20 +65,34 @@ context("Should test empirical add and open function of adr-manager", () => {
         cy.get('[data-cy=addRepoDialog]').click();
         cy.get('[data-cy=adrList]').should('have.length', 7).each(($adr, index, $adrs) => {
             cy.get($adr).click();
-            cy.get('body').then($button => {
-                if ($button.find('[data-cy=acceptDiv]').length > 0) {
+            cy.get('header').then(($a) => {
+                cy.log('text', $a.text())
+                if ($a.text().includes('Convert')) {
                     cy.log('visible')
-                    cy.readFile('cypress/fixtures/CounterDiff.json').then((counter) => {
-                        cy.writeFile('cypress/fixtures/CounterDiff.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
                     })
-                } else cy.log('not visible')
-                cy.readFile('cypress/fixtures/CounterAdrs.json').then((counter) => {
-                    cy.writeFile('cypress/fixtures/CounterAdrs.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffAllRepos.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    })
+                } else cy.log('1not visible')
+                cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                })
+                cy.readFile('cypress/fixtures/CounterAdrsAllRepos.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
                 })
             })
         })
         cy.get('[data-cy=removeRepo]').click();
         cy.get('[data-cy=removeRepoBtn]').click();
+
+        cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
+            cy.log('diff', counter.counter)
+        })
+        cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+            cy.log('adr', counter.counter)
+        })
     });
 
     it('Should test empirical add and open function of madr repo', () => {
@@ -70,25 +100,38 @@ context("Should test empirical add and open function of adr-manager", () => {
         cy.get('[data-cy=addRepoDialog]').click();
         cy.get('[data-cy=adrList]').should('have.length', 14).each(($adr, index, $adrs) => {
             cy.get($adr).click();
-            cy.get('body').then($button => {
-                if ($button.find('[data-cy=acceptDiv]').length > 0) {
+            cy.get('header').then(($a) => {
+                cy.log('text', $a.text())
+                if ($a.text().includes('Convert')) {
                     cy.log('visible')
-                    cy.readFile('cypress/fixtures/CounterDiff.json').then((counter) => {
-                        cy.writeFile('cypress/fixtures/CounterDiff.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
                     })
-                } else cy.log('not visible')
-                cy.readFile('cypress/fixtures/CounterAdrs.json').then((counter) => {
-                    cy.writeFile('cypress/fixtures/CounterAdrs.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    cy.readFile('cypress/fixtures/CounterDiffAllRepos.json').then((counter) => {
+                        cy.writeFile('cypress/fixtures/CounterDiffAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                    })
+                } else cy.log('1not visible')
+                cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsPerRepo.json', { counter: (parseInt(counter.counter) + 1).toString() });
+                })
+                cy.readFile('cypress/fixtures/CounterAdrsAllRepos.json').then((counter) => {
+                    cy.writeFile('cypress/fixtures/CounterAdrsAllRepos.json', { counter: (parseInt(counter.counter) + 1).toString() });
                 })
             })
         })
         cy.get('[data-cy=removeRepo]').click();
         cy.get('[data-cy=removeRepoBtn]').click();
 
-        cy.readFile('cypress/fixtures/CounterDiff.json').then((counter) => {
+        cy.readFile('cypress/fixtures/CounterDiffPerRepo.json').then((counter) => {
             cy.log('diff', counter.counter)
         })
-        cy.readFile('cypress/fixtures/CounterAdrs.json').then((counter) => {
+        cy.readFile('cypress/fixtures/CounterAdrsPerRepo.json').then((counter) => {
+            cy.log('adr', counter.counter)
+        })
+        cy.readFile('cypress/fixtures/CounterDiffAllRepos.json').then((counter) => {
+            cy.log('diff', counter.counter)
+        })
+        cy.readFile('cypress/fixtures/CounterAdrsAllRepos.json').then((counter) => {
             cy.log('adr', counter.counter)
         })
     });
