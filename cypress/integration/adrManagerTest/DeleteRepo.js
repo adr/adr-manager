934915783781d8b107472f0a18762c1663c5b628
Katrin <44502936+KLBa@users.noreply.github.com>
2021-01-10 00:00:13 +0100

    Adapted existing e2e tests, coverage for jest works now

--- cypress/integration/adrManagerTest/DeleteRepo.js
+++ cypress/integration/adrManagerTest/DeleteRepo.js
@@ -2,14 +2,14 @@
 
 context("Test delete repository function", () => {
     it('Should delete a repo', () => {
-        cy.visit("http://localhost:8080/manager");
         window.localStorage.clear();
         window.localStorage.setItem("authId", "8a555390-4db1-11eb-a06d-f3ebfa774e63");
+        cy.visit("http://localhost:8080/manager");
 
         // Add repo
         cy.get('[data-cy=addRepo]').click();
-        cy.intercept('GET', '**/user/repos**').as('getRepos');
-        cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
+        //cy.intercept('GET', '**/user/repos**').as('getRepos');
+        //cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
         cy.get('[data-cy=listRepo]').contains('ResearchProject').click();
         cy.get('[data-cy=addRepoDialog]').click();
         cy.get('[data-cy=repoNameList]').click();
