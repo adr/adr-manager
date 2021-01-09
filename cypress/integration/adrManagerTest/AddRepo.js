6ea1a391f5e7027779ae0a0b5e6c39d0259c5a34
unknown <st155131@stud.uni-stuttgart.de>
2021-01-09 16:17:42 +0100

    Changed test

--- cypress/integration/adrManagerTest/AddRepo.js
+++ cypress/integration/adrManagerTest/AddRepo.js
@@ -10,18 +10,19 @@ context("Test add repository", () => {
 
         cy.intercept('GET', '**/user/repos**').as('getRepos');
         cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
-
+        
         cy.get('[data-cy=listRepo]').should('have.length', 5);
         cy.get('[data-cy=listRepo]').eq(0).click();
         cy.get('[data-cy=listRepo]').eq(0).click();
         cy.get('[data-cy=listRepo]').eq(0).click();
         cy.get('[data-cy=listRepo]').eq(0).click();
         cy.get('[data-cy=listRepo]').eq(0).click();
-
+        
         cy.get('[data-cy=noRepo]').should('have.text', ' Sorry, no repositories were found! ');
-
+        
         cy.get('[data-cy=addRepoDialog]').click();
         cy.intercept('GET', '**/repos**').as('showRepos');
+        cy.wait(5000);
         cy.wait('@showRepos');
         cy.get('[data-cy=repoNameList]').children().should('have.length', 5);
         cy.get('[data-cy=repoNameList]').click();
