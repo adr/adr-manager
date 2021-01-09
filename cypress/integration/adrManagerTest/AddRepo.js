98e6150fcd60bedad34e89b97d6db695d23a5582
Daniel Abajirov <abajirovdaniel@gmail.com>
2021-01-09 14:44:12 +0100

    Second Test AddRepo.js fixed

--- cypress/integration/adrManagerTest/AddRepo.js
+++ cypress/integration/adrManagerTest/AddRepo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 context("Test add repository", () => {
-    it('Shuold add a repository', () => {
+    it.only('Shuold add a repository', () => {
         cy.visit("http://localhost:8080/manager");
         window.localStorage.clear();
         window.localStorage.setItem("authId", "8a555390-4db1-11eb-a06d-f3ebfa774e63");
@@ -11,15 +11,21 @@ context("Test add repository", () => {
         cy.intercept('GET', '**/user/repos**').as('getRepos');
         cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
 
-        cy.get('[data-cy=listRepo]').should('have.length', 1);
-        cy.get('[data-cy=listRepo]').click();
+        cy.get('[data-cy=listRepo]').should('have.length', 5);
+        cy.get('[data-cy=listRepo]').eq(0).click();
+        cy.get('[data-cy=listRepo]').eq(0).click();
+        cy.get('[data-cy=listRepo]').eq(0).click();
+        cy.get('[data-cy=listRepo]').eq(0).click();
+        cy.get('[data-cy=listRepo]').eq(0).click();
 
         cy.get('[data-cy=noRepo]').should('have.text', ' Sorry, no repositories were found! ');
 
         cy.get('[data-cy=addRepoDialog]').click();
-        cy.get('[data-cy=repoNameList]').should('have.length', 1);
+        cy.intercept('GET', '**/repos**').as('showRepos');
+        cy.wait('@showRepos');
+        cy.get('[data-cy=repoNameList]').children().should('have.length', 5);
         cy.get('[data-cy=repoNameList]').click();
 
-        cy.get('[data-cy=adrList]').should('have.length', 0);
+        cy.get('[data-cy=adrList]').contains('ResearchProject').should('have.length', 0);
     });
 })
