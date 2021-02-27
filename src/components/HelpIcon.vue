33a28ebe833c67adb86489389c8fb8aef9d85844
Katrin <44502936+KLBa@users.noreply.github.com>
2021-02-27 01:38:07 +0100

    Added explanation-on-hover icons to every section of the Editor

---
+++ src/components/HelpIcon.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-icon
+          v-on="on"
+        >mdi-help-circle-outline</v-icon>
+      </template>
+      <div style="max-width: 400px">
+        <slot />
+      </div>
+    </v-tooltip>
+</template>
+
+<script>
+
+export default {
+  name: "helpIcon",
+  components: {},
+  data: () => ({
+    user: null,
+    repositories: [],
+  }),
+  mounted() { },
+  destroyed() { },
+  methods: { },
+};
+</script>
