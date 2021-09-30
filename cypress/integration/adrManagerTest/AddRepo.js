5f463552825019ba4ad84bec0cd61ae8dbda17e4
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2021-09-30 19:55:14 +0200

    Restructuring of e2e test logic and reworking of first tests

--- cypress/integration/adrManagerTest/AddRepo.js
+++ cypress/integration/adrManagerTest/AddRepo.js
@@ -1,58 +1,77 @@
 /* eslint-disable no-undef */
-import { authId } from "./constants.js"
 
-context("Test add repository", () => {
+context("Listing and adding repositories", () => {
     beforeEach(() => {
         window.localStorage.clear();
-        window.localStorage.setItem("authId", authId);
+        window.localStorage.setItem(
+            "authId",
+            Cypress.env("PIZZLY_E2E_AUTH_ID")
+        );
         cy.visit("http://localhost:8080/#/manager");
 
-        cy.intercept('GET', '**/user/repos**').as('getRepos');
-        cy.get('[data-cy=addRepo]').click();
-        cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
+        cy.intercept("GET", "**/user/repos**").as("getRepos");
+        cy.get("[data-cy=addRepo]").click();
+        cy.wait("@getRepos")
+            .its("response.statusCode")
+            .should("eq", 200);
     });
 
-    // This is the only test that relies on having exactly 5 repositories.
-    it('Check if all repositories are displayed', () => {
-        cy.get('[data-cy=listRepo]').should('have.length', 5);
+    it("Check if at least 1 repository is displayed", () => {
+        cy.get("[data-cy=listRepo]").should("have.length.greaterThan", 0);
     });
 
-    it('Test adding all repositories', () => {
-        cy.get('[data-cy=listRepo]')
-            .then(listing => {
-                const listingCount = Cypress.$(listing).length;
-                cy.get('[data-cy=listRepo]').each(() => {
-                    cy.get('[data-cy=listRepo]').eq(0).click();
-                });
-
-                cy.get('[data-cy=noRepo]').should('have.text', ' Sorry, no repositories were found! ');
-
-                cy.get('[data-cy=addRepoDialog]').click();
-                cy.intercept('GET', '**/repos**').as('showRepos');
-                cy.wait('@showRepos', {timeout: 15000});
-
-                cy.get('[data-cy=repoNameList]').children().should('have.length', listingCount);
-                cy.get('[data-cy=repoNameList]').click();
-
-                cy.get('[data-cy=adrList]').contains('ResearchProject').should('have.length', 0);
+    it("Add all repositories", () => {
+        cy.get("[data-cy=listRepo]").then(listing => {
+            const listingCount = Cypress.$(listing).length;
+            // add each repo with a click
+            cy.get("[data-cy=listRepo]").each(() => {
+                cy.get("[data-cy=listRepo]")
+                    .eq(0)
+                    .click();
             });
+
+            // check if the text for no further repos is displayed
+            cy.get("[data-cy=noRepo]").should(
+                "have.text",
+                " Sorry, no repositories were found! "
+            );
+
+            // confirm repo adding dialog
+            cy.get("[data-cy=addRepoDialog]").click();
+            cy.intercept("GET", "**/repos**").as("showRepos");
+            cy.wait("@showRepos", { timeout: 15000 });
+
+            // check if the correct number of repos was added
+            cy.get("[data-cy=repoNameList]")
+                .children()
+                .should("have.length", listingCount);
+        });
     });
 
-    it('Test adding three repositories', () => {
-        cy.get('[data-cy=listRepo]').should('have.length.greaterThan', 2);
+    it("Add three repositories", () => {
+        cy.get("[data-cy=listRepo]").should("have.length.greaterThan", 2);
 
-        cy.get('[data-cy=listRepo]').eq(0).click();
-        cy.get('[data-cy=listRepo]').eq(0).click();
-        cy.get('[data-cy=listRepo]').eq(0).click();
+        cy.get("[data-cy=listRepo]")
+            .eq(0)
+            .click();
+        cy.get("[data-cy=listRepo]")
+            .eq(0)
+            .click();
+        cy.get("[data-cy=listRepo]")
+            .eq(0)
+            .click();
 
-        cy.get('[data-cy=addRepoDialog]').click();
-        cy.intercept('GET', '**/repos**').as('showRepos');
-        cy.wait('@showRepos', {timeout: 10000});
+        cy.get("[data-cy=addRepoDialog]").click();
+        cy.intercept("GET", "**/repos**").as("showRepos");
+        cy.wait("@showRepos", { timeout: 10000 });
 
-        cy.get('[data-cy=repoNameList]').children().should('have.length', 3);
-        cy.get('[data-cy=repoNameList]').click();
+        cy.get("[data-cy=repoNameList]")
+            .children()
+            .should("have.length", 3);
+        cy.get("[data-cy=repoNameList]").click();
 
-        cy.get('[data-cy=adrList]').contains('ResearchProject').should('have.length', 0);
+        cy.get("[data-cy=adrList]")
+            .contains("ResearchProject")
+            .should("have.length", 0);
     });
-
-})
+});
--- cypress/integration/adrManagerTest/DeleteRepo.js
+++ cypress/integration/adrManagerTest/DeleteRepo.js
@@ -1,27 +1,33 @@
 /* eslint-disable no-undef */
-import { authId } from "./constants.js"
 
 context("Test delete repository function", () => {
-    it('Should delete a repo', () => {
+    it("Should delete a repo", () => {
         window.localStorage.clear();
-        window.localStorage.setItem("authId", authId);
+        window.localStorage.setItem(
+            "authId",
+            Cypress.env("PIZZLY_E2E_AUTH_ID")
+        );
         cy.visit("http://localhost:8080/#/manager");
 
         // Add repo
-        cy.intercept('GET', '**/user/repos**').as('getRepos');
-        cy.get('[data-cy=addRepo]').click();
-        cy.wait('@getRepos').its('response.statusCode').should('eq', 200);
-        
-        cy.get('[data-cy=listRepo]').contains('ResearchProject').click();
-        cy.get('[data-cy=addRepoDialog]').click();
-        cy.get('[data-cy=repoNameList]').click();
+        cy.intercept("GET", "**/user/repos**").as("getRepos");
+        cy.get("[data-cy=addRepo]").click();
+        cy.wait("@getRepos")
+            .its("response.statusCode")
+            .should("eq", 200);
+
+        cy.get("[data-cy=listRepo]")
+            .contains("ResearchProject")
+            .click();
+        cy.get("[data-cy=addRepoDialog]").click();
+        cy.get("[data-cy=repoNameList]").click();
 
         // Delete repo
-        cy.get('[data-cy=removeRepo]').click();
-        cy.get('[data-cy=removeRepoBtn]').click();
-        cy.get('[data-cy=listRepo]').should('have.length', 0);
-        cy.get('[data-cy=addRepo]').should(() => {
-            expect(localStorage.getItem('addedRepositories')).to.eq('[]');
-        })
+        cy.get("[data-cy=removeRepo]").click();
+        cy.get("[data-cy=removeRepoBtn]").click();
+        cy.get("[data-cy=listRepo]").should("have.length", 0);
+        cy.get("[data-cy=addRepo]").should(() => {
+            expect(localStorage.getItem("addedRepositories")).to.eq("[]");
+        });
     });
-})
+});
