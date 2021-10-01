d64c2e6717229a25985c5a78479f5ec4e4fd2f9f
Justus Bogner <justus.bogner@gmail.com>
2021-10-01 22:34:04 +0200

    Finalized e2e test reworking; move unit tests to own CI workflow

--- cypress/integration/adrManagerTest/PushNewAdr.js
+++ cypress/integration/adrManagerTest/PushNewAdr.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
-/// <reference types="cypress" />
 
-context("Test commit and push file --> delete file in repo (push)", () => {
-    it("Should create a new file, then push it to GitHub. Then delete file in GitHub", () => {
+context("Committing, pushing, and remote-deleting an ADR", () => {
+    it("Commit and push new ADR, then delete from GitHub", () => {
         window.localStorage.clear();
         window.localStorage.setItem(
             "authId",
@@ -10,48 +9,28 @@ context("Test commit and push file --> delete file in repo (push)", () => {
         );
         cy.visit("http://localhost:8080/#/manager");
 
-        // Add repo
+        // add the ADR-Manager repo
         cy.intercept("GET", "**/user/repos**").as("getRepos");
         cy.get("[data-cy=addRepo]").click();
         cy.wait("@getRepos")
             .its("response.statusCode")
             .should("eq", 200);
-
         cy.get("[data-cy=listRepo]")
-            .should("have.length", 5)
-            .eq(0)
+            .contains("ADR-Manager")
             .click();
-
-        cy.intercept("GET", "**/repos**").as("showRepos");
         cy.get("[data-cy=addRepoDialog]").click();
-        cy.intercept("GET", "**/repos**").as("showRepos");
-        cy.wait("@showRepos", { timeout: 10000 });
 
-        // If the repository is not expanded automatically, click on it.
-        cy.get("body").then($body => {
-            if ($body.find("[data-cy=newADR]").length > 0) {
-                // if button exists at all
-                cy.get("[data-cy=newADR]").then($header => {
-                    if (!$header.is(":visible")) {
-                        // if button is INVISIBLE
-                        cy.get("[data-cy=repoNameList]").click();
-                    }
-                });
-            } else {
-                //if the button DOESN'T EXIST
-                cy.get("[data-cy=repoNameList]").click();
-            }
-        });
+        cy.get("[data-cy=repoNameList]").click();
 
-        // Add new file
+        // add new ADR
         cy.get("[data-cy=NewAdrFile]").click({ force: true });
-        cy.contains("0001-*").click();
+        // initiate commit dialog
         cy.get("[data-cy=pushIcon]").click();
-
-        // Check dialog commit & push
+        // check for dialog
         cy.get("[data-cy=btnOfDialogCommitForPush]").should("be.disabled");
         cy.get("[data-cy=mdiAlertNotSelected]").should("be.visible");
         cy.get("[data-cy=mdiAlertCommitMessage]").should("be.visible");
+        // set commit message and commit
         cy.get("[data-cy=newFilesCommitMessage]").click();
         cy.get("[data-cy=newFileCheckBox]").check({ force: true });
         cy.get("[data-cy=mdiCheckSelected]").should("be.visible");
@@ -59,15 +38,19 @@ context("Test commit and push file --> delete file in repo (push)", () => {
             "Commit Message to add a new ADR."
         );
         cy.get("[data-cy=mdiCheckCommitMessage]").should("be.visible");
+        // push to GitHub
         cy.get("[data-cy=btnOfDialogCommitForPush]").click();
         cy.intercept("POST", "**/github/repos**").as("getRepos");
         cy.wait("@getRepos");
         cy.contains("OK").click();
-        cy.get("[data-cy=deleteAdrBtn]").click();
+        // delete the ADR
+        cy.get("[data-cy=deleteAdrBtn]")
+            .last()
+            .click();
         cy.get("[data-cy=dialogDeleteAdrBtn").click();
 
-        // Delete AdrFile
-        cy.wait(60000);
+        // commit and push
+        cy.wait(62000);
         cy.get("[data-cy=pushIcon]").click();
         cy.get("[data-cy=deletedFilesAdr]").click();
         cy.get("[data-cy=deletedFileCheckBox]").check({ force: true });
--- cypress/integration/adrManagerTest/Routing.js
+++ cypress/integration/adrManagerTest/Routing.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
 
-context("Test for correct URLs", () => {
+context("Routing and correct URLs", () => {
     beforeEach(() => {
-        // Clean up local storage
         window.localStorage.clear();
         window.localStorage.setItem(
             "authId",
@@ -11,74 +10,55 @@ context("Test for correct URLs", () => {
         cy.visit("http://localhost:8080/#/manager");
     });
 
-    it("URL should correspond to opened repo/ADR.", () => {
+    it("URL corresponds to opened repo and ADR", () => {
         cy.url().should("equal", "http://localhost:8080/#/manager");
 
+        // add the ADR-Manager repo
         cy.intercept("GET", "**/user/repos**").as("getRepos");
         cy.get("[data-cy=addRepo]").click();
         cy.wait("@getRepos")
             .its("response.statusCode")
             .should("eq", 200);
-
-        // Add one repository.
-        cy.get("[data-cy=listRepo]").should("have.length", 5);
-        cy.contains("researchproject2020/ResearchProject").click();
-
+        cy.get("[data-cy=listRepo]")
+            .contains("ADR-Manager")
+            .click();
         cy.get("[data-cy=addRepoDialog]").click();
-        cy.intercept("GET", "**/repos**").as("showRepos");
-        cy.wait("@showRepos", { timeout: 10000 });
 
-        // If the repository is not expanded automatically, click on it.
-        cy.get("body").then($body => {
-            if ($body.find("[data-cy=newADR]").length > 0) {
-                //if button exists at all
-                cy.get("[data-cy=newADR]").then($header => {
-                    if (!$header.is(":visible")) {
-                        //if button is INVISIBLE
-                        cy.get("[data-cy=repoNameList]").click();
-                    }
-                });
-            } else {
-                // if the button DOESN'T EXIST
-                cy.get("[data-cy=repoNameList]").click();
-            }
+        // URL should contain full name, branch, and ADR name
+        cy.url().should(
+            "equal",
+            "http://localhost:8080/#/manager/adr/adr-manager/main/0000-use-markdown-architectural-decision-records.md"
+        );
+
+        cy.get("[data-cy=adrList]").then(adrList => {
+            // get number of ADRs in repo
+            const adrCount = Cypress.$(adrList).length;
+            expect(adrCount).to.be.greaterThan(3);
+            // add new ADR, it should be opened automatically
+            cy.get("[data-cy=newADR]").click({ force: true });
+            // with an opened ADR, the URL should contain full name, branch, and new ADR name
+            cy.url().should(
+                "equal",
+                `http://localhost:8080/#/manager/adr/adr-manager/main/${String(
+                    adrCount
+                ).padStart(4, "0")}-.md`
+            );
+
+            // remove all ADRs
+            cy.get("[data-cy=deleteAdrBtn]").each($el => {
+                cy.wrap($el).click();
+                cy.get("[data-cy=dialogDeleteAdrBtn]").click();
+            });
+
+            // URL should contain full name and branch
+            cy.url().should(
+                "equal",
+                "http://localhost:8080/#/manager/adr/adr-manager/main"
+            );
         });
-
-        // Assuming the repo does not contain an ADR, the URL should contain: full name and branch.
-        cy.url().should(
-            "equal",
-            "http://localhost:8080/#/manager/researchproject2020/ResearchProject/main"
-        );
-        cy.get("[data-cy=adrList]").should("have.length", 0);
-
-        // Add one ADR to list, this ADR should be opened automatically.
-        cy.get("[data-cy=newADR]").click({ force: true });
-
-        // With an opened ADR, the URL should contain: full name, branch, adr name.
-        cy.url().should(
-            "equal",
-            "http://localhost:8080/#/manager/researchproject2020/ResearchProject/main/0001-.md"
-        );
-
-        cy.get("[data-cy=adrList]")
-            .contains("ResearchProject")
-            .should("have.length", 0);
-
-        // Delete the ADR
-        cy.get("[data-cy=deleteAdrBtn]").click();
-        cy.get("[data-cy=dialogDeleteAdrBtn]").click();
-        cy.get("[data-cy=adrList]").should("have.length", 0);
-
-        // The repo does not contain an ADR again, so the URL should contain: full name and branch.
-        cy.url().should(
-            "equal",
-            "http://localhost:8080/#/manager/researchproject2020/ResearchProject/main"
-        );
-
-        cy.get("[data-cy=removeRepo]").click();
     });
 
-    it("If the local storage does not contain the auth ID, redirect to /login.", () => {
+    it("Redirect to /login if localStorage does not contain the authId", () => {
         cy.url().should("contain", "http://localhost:8080/#/manager");
         cy.clearLocalStorage();
         cy.reload();
