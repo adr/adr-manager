d1a383962ea7d00a9a907c4ba4e5fa3dda4084e3
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2021-10-06 16:49:37 +0200

    Updated libraries and fixed breaking changes; refined UI element margins

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  presets: [
-    "@vue/cli-plugin-babel/preset"
-  ],
-  env: {
-    e2e: { // This is needed otherwise there's a conflict with Jest
-      plugins: [
-        ["babel-plugin-istanbul", {
-          extension: [".js", ".vue"]
-        }]
-      ]
+    presets: ["@vue/cli-plugin-babel/preset"],
+    env: {
+        e2e: {
+            // This is needed otherwise there's a conflict with Jest
+            plugins: [
+                [
+                    "babel-plugin-istanbul",
+                    {
+                        extension: [".js", ".vue"]
+                    }
+                ]
+            ]
+        }
     }
-  }
 };
--- src/components/ToolbarMenuMode.vue
+++ src/components/ToolbarMenuMode.vue
@@ -1,60 +1,71 @@
 <template>
-  <div>
-    <v-tabs v-model="tab" class="mx-0 px-0 pt-0 mt-0 flex-grow-0" background-color="transparent">
-      <div class="align-self-center pr-4" >
-        Editor Mode: 
-      </div>
-      <v-tooltip v-for="item in modes" :key="item.name" open-delay="500" bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-tab text v-on:click="setMode(item.name)" v-on="on" v-bind="attrs">
-            {{ item.name }}
-          </v-tab>
-        </template>
-        <span>
-          {{ item.tooltip }}
-        </span>
-      </v-tooltip>
-    </v-tabs>
-  </div>
+    <div>
+        <v-tabs
+            v-model="tab"
+            class="mx-0 px-0 pt-0 mt-0 flex-grow-0"
+            background-color="transparent"
+        >
+            <div class="align-self-center pr-4">
+                Editor Mode:
+            </div>
+            <v-tooltip
+                v-for="item in modes"
+                :key="item.name"
+                open-delay="500"
+                bottom
+            >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-tab
+                        text
+                        v-on:click="setMode(item.name)"
+                        v-on="on"
+                        v-bind="attrs"
+                    >
+                        {{ item.name }}
+                    </v-tab>
+                </template>
+                <span>
+                    {{ item.tooltip }}
+                </span>
+            </v-tooltip>
+        </v-tabs>
+    </div>
 </template>
 
 <script>
-  import { store } from "@/plugins/store.js"
+import { store } from "@/plugins/store.js";
 
-  export default {
+export default {
     name: "MenuMode",
-    components: {
-    },
+    components: {},
     data: () => ({
-      tab: 0,
-      modes: [
-        { name: "basic", tooltip: "Only show required fields." },
-        { name: "professional", tooltip: "Show all fields." }
-      ]
+        tab: 0,
+        modes: [
+            { name: "basic", tooltip: "Only show required fields." },
+            { name: "professional", tooltip: "Show all fields." }
+        ]
     }),
     props: {
-      consideredOptions: {
-        type: Array, // Array
-        required: false,
-        default: () => ([])
-      }
+        consideredOptions: {
+            type: Array, // Array
+            required: false,
+            default: () => []
+        }
     },
-    computed: {
-    },
+    computed: {},
     created() {
-      this.tab = this.modes.findIndex(m => (m.name === store.mode));
-      store.$on("set-mode", this.updateModeLocal);
+        this.tab = this.modes.findIndex(m => m.name === store.mode);
+        store.$on("set-mode", this.updateModeLocal);
     },
     methods: {
-      updateModeLocal(mode) {
-        this.tab = this.modes.findIndex(m => (m.name === mode));
-      },
-      setMode(mode) {
-        store.setMode(mode);
-      }
+        updateModeLocal(mode) {
+            this.tab = this.modes.findIndex(m => m.name === mode);
+        },
+        setMode(mode) {
+            store.setMode(mode);
+        }
     }
-  };
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
