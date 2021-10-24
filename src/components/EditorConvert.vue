404ca3e70bf030c70ad3e7b10f2861101b0b7081
Justus Bogner <justus.bogner@gmail.com>
2021-10-24 14:10:37 +0200

    Updated message for parsing errors

--- src/components/EditorConvert.vue
+++ src/components/EditorConvert.vue
@@ -6,7 +6,7 @@
         style="height: 100%"
         data-cy="convertEditor"
     >
-        <v-card-title> Sorry, we have parsing trouble. </v-card-title>
+        <v-card-title> Sorry, there were issues while parsing the ADR.</v-card-title>
         <div>
             If you want to use the MADR-Editor, our parser will generate the
             markdown on the right-hand side. You can edit your raw Markdown to
