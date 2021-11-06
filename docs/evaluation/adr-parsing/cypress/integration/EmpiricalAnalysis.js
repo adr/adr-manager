b5408828d006de2ba5addc8705828be971a58733
Katrin <44502936+KLBa@users.noreply.github.com>
2021-11-06 16:43:21 +0100

    Update parser test in docs/evaluation/adr-parsing
    See README for a How-To.
    
    See cypress/videos/EmpiricalAnalysis.js.mp4 for how it **should** look
     and cypress/fixtures for counters corresponding to the video.

---
+++ docs/evaluation/adr-parsing/cypress/integration/EmpiricalAnalysis.js
@@ -0,0 +1,162 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+var REPO_NAMES = [ // TODO: Update with your repository names.
+    "adr-madr",
+    "adr-adr-log",
+    "api-platform-core",
+    "archivematica-archivematica-architectural-decisions",
+    "bbc-digital-paper-edit-client",
+    "bbc-digital-paper-edit-firebase",
+    "change-metrics-monocle",
+    "dante-ev-docker-texlive",
+    "eclipse-winery",
+    "elastic-cloud-on-k8s",
+    "elastisys-compliantkubernetes",
+    "Enalean-tuleap",
+    "island-is-island-is",
+    "JabRef-jabref",
+    "knizamov-ecar-charge-pricing",
+    "mozilla-experimenter",
+    "mozilla-fxa",
+    "nodejs-nodejs.dev",
+    "npryce-adr-tools",
+    "opendatahub-io-odh-manifests",
+    "operate-first-blueprint",
+    "opinionated-digital-center-architecture-decision-records",
+    "raiden-network-raiden",
+    "raiden-network-raiden-services",
+    "sylius-sylius",
+    "theupdateframework-python-tuf",
+    "thomvaill-log4brains",
+    "UST-MICO-docs",
+]
+
+context("Empirically test if ADRs can be opened by the ADR-Manager and count how many are accepted by the parser without the 'Diff' screen.", () => {
+
+    beforeEach(() => {
+        cy.visit("http://localhost:8080/#/manager");
+        window.localStorage.clear();
+        window.localStorage.setItem(
+            "authId",
+            Cypress.env("PIZZLY_E2E_AUTH_ID")
+        );
+
+        cy.intercept("GET", "**/user/repos**").as("getRepos");
+        cy.get("[data-cy=addRepo]").click();
+        cy.wait("@getRepos")
+            .its("response.statusCode")
+            .should("eq", 200);
+
+    });
+
+    for (var j = 0; j < REPO_NAMES.length; j++) {
+        let i = j // Store loop variable into the local scope.
+        function testDiff() {
+            if (i == 0) {
+                // In the first iteration create counters
+                cy.writeFile("cypress/fixtures/CounterDiffAllRepos.json", {
+                    counter: 0
+                });
+                cy.writeFile("cypress/fixtures/CounterAdrsAllRepos.json", {
+                    counter: 0
+                });
+                cy.writeFile("cypress/fixtures/CounterDiffPerRepo.json", {
+                    [REPO_NAMES[i]]: 0
+                });
+                cy.writeFile("cypress/fixtures/CounterAdrsPerRepo.json", {
+                    [REPO_NAMES[i]]: 0
+                });
+            } else {
+                // Add repo-specific counters
+                cy.readFile(
+                    "cypress/fixtures/CounterAdrsPerRepo.json"
+                ).then(counter => {
+                    counter[REPO_NAMES[i]] = 0;
+                    cy.writeFile("cypress/fixtures/CounterAdrsPerRepo.json", counter);
+                });
+                cy.readFile(
+                    "cypress/fixtures/CounterDiffPerRepo.json"
+                ).then(counter => {
+                    counter[REPO_NAMES[i]] = 0;
+                    cy.writeFile("cypress/fixtures/CounterDiffPerRepo.json", counter);
+                });
+            }
+            // add the repo
+            cy.log(REPO_NAMES[i])
+            cy.get("[data-cy=listRepo]")
+                .contains(REPO_NAMES[i])
+                .click();
+            cy.get("[data-cy=addRepoDialog]").click({ force: true });
+            cy.intercept("GET", "**/repos**").as("showRepos");
+            cy.wait("@showRepos", { timeout: 10000 });
+
+            // Iterate through the ADRs and count how often the Convert Tab is opened.
+            cy.get("[data-cy=adrList]")
+                .each(($adr, index, $adrs) => {
+                    cy.get($adr).click();
+                    cy.get("header").then($a => {
+                        cy.log("text", $a.text());
+                        if ($a.text().includes("Convert")) {
+                            cy.log("Convert Tab opened");
+                            // Update Diff counters
+                            cy.readFile(
+                                "cypress/fixtures/CounterDiffPerRepo.json"
+                            ).then(counter => {
+                                counter[REPO_NAMES[i]] = counter[REPO_NAMES[i]] + 1;
+                                cy.writeFile(
+                                    "cypress/fixtures/CounterDiffPerRepo.json",
+                                    counter
+                                );
+                            });
+                            cy.readFile(
+                                "cypress/fixtures/CounterDiffAllRepos.json"
+                            ).then(counter => {
+                                counter.counter = counter.counter + 1;
+                                cy.writeFile(
+                                    "cypress/fixtures/CounterDiffAllRepos.json",
+                                    counter
+                                );
+                            });
+                        } else cy.log(i + " not visible");
+
+                        // Update ADR counters
+                        cy.readFile(
+                            "cypress/fixtures/CounterAdrsPerRepo.json"
+                        ).then(counter => {
+                            counter[REPO_NAMES[i]] = counter[REPO_NAMES[i]] + 1;
+                            cy.writeFile(
+                                "cypress/fixtures/CounterAdrsPerRepo.json",
+                                counter
+                            );
+                        });
+                        cy.readFile(
+                            "cypress/fixtures/CounterAdrsAllRepos.json"
+                        ).then(counter => {
+                            counter.counter = counter.counter + 1;
+                            cy.writeFile(
+                                "cypress/fixtures/CounterAdrsAllRepos.json",
+                                counter
+                            );
+                        });
+                    });
+                });
+            cy.get("[data-cy=removeRepo]").click();
+            cy.get("[data-cy=removeRepoBtn]").click();
+
+            cy.readFile("cypress/fixtures/CounterDiffPerRepo.json").then(
+                counter => {
+                    cy.log("diff", counter.counter);
+                }
+            );
+            cy.readFile("cypress/fixtures/CounterAdrsPerRepo.json").then(
+                counter => {
+                    cy.log("adr", counter.counter);
+                }
+            );
+        }
+
+        // Create the actual tests
+        it("Test ADRs of the repository " + REPO_NAMES[i], testDiff);
+    }
+});
